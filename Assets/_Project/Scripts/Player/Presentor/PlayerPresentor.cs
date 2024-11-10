feab51ea5c6bed77cadfa1bf1c00a7ea62789988
Evgeniy Ignatev <osero6110@gmail.com>
2024-11-10 16:21:49 +0300

    Updated view and presenter

--- Assets/_Project/Scripts/Player/Presentor/PlayerPresentor.cs
+++ Assets/_Project/Scripts/Player/Presentor/PlayerPresentor.cs
@@ -1,5 +1,14 @@
+using Zenject;
+
 public class PlayerPresentor
 {
+    private IPlayerView _playerView;
 
-       
+    public PlayerPresentor(IPlayerView playerView) => 
+        _playerView = playerView;
+
+    public void Debuging()
+    {
+        UnityEngine.Debug.Log($"_playerView: {_playerView}");
+    }
 }
--- Assets/_Project/Scripts/Player/View/PlayerView.cs
+++ Assets/_Project/Scripts/Player/View/PlayerView.cs
@@ -1,5 +1,23 @@
 using UnityEngine;
+using Zenject;
 
-public class PlayerView : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerView : MonoBehaviour, IPlayerView
 {
+    private Rigidbody2D _rigidbody;
+    private PlayerPresentor _presentor;
+
+    private void Awake() => 
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+    [Inject]
+    public void Construct(PlayerPresentor presentor) => 
+        _presentor = presentor;
+
+    private void Update()
+    {
+        Debug.Log($"_presentor: {_presentor}");
+
+        _presentor.Debuging();
+    }
 }
